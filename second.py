fe042867ff424fc15518a0111c743bab9440a4ea
h-attila <email.atinak@gmail.com>
2016-12-05 09:40:09 +0100

    second ok

--- second.py
+++ second.py
@@ -4,3 +4,22 @@
 # If any problem raises with the file output, the function should not break, but return False.
 # Example: when called with the following two parameters: "tree.txt", "apple",
 # the function should write "appleappleapple" to the file "tree.txt", and return True.
+
+
+def string_writer(fileName, inputString):
+
+    if type(inputString) == str:
+        try:
+            inputFile = open(fileName, 'w')
+            newString = inputString * 10
+            inputFile.write(newString)
+            inputFile.close()
+            return True
+        except:
+            return False
+    else:
+        raise TypeError('only string type input accepted!')
+
+
+print(string_writer('tree.txt', 'apple'))
+print(string_writer('tree.txt', 1234))          #test by fake data
